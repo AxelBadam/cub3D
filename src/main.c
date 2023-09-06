62a4830ed4ebe40f3352a653eda3661f81c3567e
John Doe <johndoe@example.com>
2023-09-06 12:03:45 +0300

    merge

--- src/main.c
+++ src/main.c
@@ -6,25 +6,14 @@
 /*   By: atuliara <atuliara@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 16:05:22 by ekoljone          #+#    #+#             */
-/*   Updated: 2023/09/06 11:52:42 by atuliara         ###   ########.fr       */
+/*   Updated: 2023/09/06 11:54:07 by atuliara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 void	update(void *param)
-void	update(void *param)
 {
-	t_cubed	*cubed;
-
-	cubed = param;
-	if (cubed->mouse == 1)
-		mlx_set_cursor_mode(cubed->mlx.mlx, MLX_MOUSE_NORMAL);
-	if (cubed->mouse == 0)
-		mlx_set_cursor_mode(cubed->mlx.mlx, MLX_MOUSE_HIDDEN);
-	if (cubed->mouse == 0)
-		mouse_rotate(cubed);
-	check_keys(cubed);
 	t_cubed	*cubed;
 
 	cubed = param;
@@ -48,7 +37,6 @@ void	cub3d(t_cubed *cubed)
 	load_text(cubed);
 	draw(cubed);
 	mlx_loop_hook(cubed->mlx.mlx, &update, cubed);
-	mlx_loop_hook(cubed->mlx.mlx, &update, cubed);
 	mlx_loop(cubed->mlx.mlx);
 	mlx_terminate(cubed->mlx.mlx);
 }
@@ -56,16 +44,9 @@ void	cub3d(t_cubed *cubed)
 int	main(int argc, char **argv)
 {
 	t_cubed	cubed;
-	t_cubed	cubed;
 
 	if (argc != 2)
 		return (1);
-	cubed.north = NULL;
-	cubed.south = NULL;
-	cubed.west = NULL;
-	cubed.east = NULL;
-	cubed.mlx.mlx = NULL;
-	cubed.mlx.image = NULL;
 	cubed.north = NULL;
 	cubed.south = NULL;
 	cubed.west = NULL;
